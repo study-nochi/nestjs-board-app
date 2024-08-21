c0927d49e3bbb27f9f554873576b8205af5d142e
ainochi-kor <kthrkdals@gmail.com>
2024-08-22 03:03:21 +0900

    게시물 생성하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,17 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC, // default status
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
