0fbed90eb28d514ba806fa31495130cace5d1ac9
ainochi-kor <kthrkdals@gmail.com>
2024-09-04 23:56:17 +0900

    유저 데이터 유효성 체크

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
-  usename: string;
-  password: string
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  //영어랑 숫자만 가능한 정규식 추가
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only accepts english and number',
+  })
+  password: string;
 }
